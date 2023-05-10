ce07ccda8212307648014d80097280682bf4eb33
Sergio Peralta <seperalta88@gmail.com>
2023-05-10 14:28:05 -0300

    Create add function test

---
+++ src/tests/addRemove.test.js
@@ -0,0 +1,30 @@
+import addTask from '../modules/addTask.js';
+import taskArray from '../modules/taskArray.js';
+
+document.body.innerHTML = `<div><input type="text" class="input" id="add-input" placeholder="Add to your list...">
+<div class="list-container">
+<ul class="list"></ul>
+</div></div>`;
+
+describe('add method', () => {
+  test('should add a item to the list', () => {
+    addTask('hello World');
+    addTask('hello Microverse');
+    const list = document.querySelectorAll('.task-label');
+    expect(list.length).toEqual(2);
+  });
+  test('should change the value of taskArray', () => {
+    expect(taskArray).toEqual([
+      {
+        description: 'hello World',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'hello Microverse',
+        completed: false,
+        index: 2,
+      },
+    ]);
+  });
+});
